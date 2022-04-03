e2c01ac031e806c30ca85f1239449ef325be85c4
Robert Coffey <rjc@robertcoffey.xyz>
2022-04-02 23:25:51 -0230

    rewrite from scratch

--- Makefile
+++ Makefile
@@ -1,5 +1,11 @@
+.PHONY: all
 all:
 	ghc words.hs
 
+.PHONY: install
 install:
 	cp words /usr/local/bin
+
+.PHONY: uninstall
+uninstall:
+	rm /usr/local/bin/words
--- words.hs
+++ words.hs
@@ -1,82 +1,21 @@
-{- words.hs - Get a list of words and their counts from a file. -}
+-- words.hs - print wordlist and their counts from stdin
+-- Copyright (C) 2022 Robert Coffey
+-- Released under the GPLv3.
 
-import System.Environment
+import Data.Function
+import Data.List
+
+wordList :: String -> [[String]]
+wordList str = group . sort $ words str
+
+wordCount :: [[String]] -> [(String, Int)]
+wordCount lst = zip (map head lst) (map length lst)
+
+wordString :: [(String, Int)] -> String
+wordString lst = unlines $ map (\e -> (fst e) ++ "\t" ++ (show . snd $ e)) lst
 
 main :: IO ()
-main = do
-  args <- getArgs
-  mapM_ countFile args
-
-countFile :: String -> IO ()
-countFile path = do
-  content <- readFile path
-  printWordCount . sort . countWords . words $ content
-
-printWordCount :: (Num n, Show n) => [(String, n)] -> IO ()
-printWordCount [] = return ()
-printWordCount (x:xs) = do
-  putStr ((fst x) ++ ":\t")
-  print (snd x)
-  printWordCount xs
-
-sort :: (Num n, Ord n) => [(String, n)] -> [(String, n)]
-sort [] = []
-sort l = sortByCount . sortByWord $ l
-
-sortByWord :: Num n => [(String, n)] -> [(String, n)]
-sortByWord [] = []
-sortByWord (x:xs) =
-  insertByWord x (sortByWord xs)
-
-insertByWord :: Num n => (String, n) -> [(String, n)] -> [(String, n)]
-insertByWord t [] = [t]
-insertByWord t (x:xs) =
-  if (fst t) > (fst x)
-  then t : x : xs
-  else x : insertByWord t xs
-
-sortByCount :: (Num n, Ord n) => [(String, n)] -> [(String, n)]
-sortByCount [] = []
-sortByCount (x:xs) =
-  insertByCount x (sortByCount xs)
-
-insertByCount :: (Num n, Ord n) => (String, n) -> [(String, n)] -> [(String, n)]
-insertByCount t [] = [t]
-insertByCount t (x:xs) =
-  if (snd t) > (snd x)
-  then t : x : xs
-  else x : insertByCount t xs
-
-countWords :: Num n => [String] -> [(String, n)]
-countWords l = countWords' l []
-
-countWords' :: Num n => [String] -> [(String, n)] -> [(String, n)]
-countWords' [] l = l
-countWords' (x:xs) l = countWords' xs (keepFirst x (addWord x l))
-
-keepFirst :: Num n => String -> [(String, n)] -> [(String, n)]
-keepFirst w [] = []
-keepFirst w (x:xs) =
-  if w == fst x
-  then x : dropWord w xs
-  else x : keepFirst w xs
-
-dropWord :: Num n => String -> [(String, n)] -> [(String, n)]
-dropWord w [] = []
-dropWord w (x:xs) =
-  if w == fst x
-  then xs
-  else x : dropWord w xs
-
-addWord :: Num n => String -> [(String, n)] -> [(String, n)]
-addWord w l =
-  case findPair w l of
-    Just pair -> (fst pair, (snd pair)+1) : l
-    Nothing -> (w, 1) : l
-
-findPair :: Eq a => a -> [(a, b)] -> Maybe (a, b)
-findPair t [] = Nothing
-findPair t (x:xs) =
-  if t == fst x
-  then Just x
-  else findPair t xs
+main = interact $ wordString
+                . sortBy (flip (compare `on` snd))
+                . wordCount
+                . wordList
